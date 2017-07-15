6a7dfd326e1bcebe68308bdb84d36570053d3886
Glauco Roberto <glaucomunsberg@gmail.com>
2017-07-15 19:27:25 -0300

    so... better now!

--- anima/Callbacks.py
+++ anima/Callbacks.py
@@ -13,7 +13,8 @@ class JSONMetrics(callbacks.Callback):
     _config     = None
     _model      = None
     _each_epoch = None
-    metrics     = None
+    _metrics    = None
+    _epoch      = None
     
     def __init__(self,model,each_epoch):
         self._helper    = Helper()
@@ -23,28 +24,27 @@ class JSONMetrics(callbacks.Callback):
         self._file_json = self._config.output_folder+self._serial+"_menesis_metrics.json"
         self._model     = model
         self._each_epoch= each_epoch
-        self._batch     = 0
-        self.metrics    = {'loss':[], 'acc':[]}
+        self._epoch     = 0
+        self._metrics   = {'loss':[], 'acc':[]}
         
     def on_epoch_begin(self, epoch, logs):
         try:
             with open(self._file_json, 'r') as f:   
-                self.metrics = json.load(f)
+                self._metrics = json.load(f)
         except:
             with open(self._file_json, 'w') as f:
-                self.metrics = {'loss':[], 'acc':[]}
-                json.dump(self.metrics, f)
+                self._metrics = {'loss':[], 'acc':[]}
+                json.dump(self._metrics, f)
  			
     def on_epoch_end(self, epoch, logs):
-        self.metrics['loss'].append(logs.get('loss'))
-        self.metrics['acc'].append(logs.get('acc'))
+        self._metrics['loss'].append(logs.get('loss'))
+        self._metrics['acc'].append(logs.get('acc'))
         with open(self._file_json, 'w') as f:
-            data = json.dump(self.metrics, f)
-    
-    def on_batch_end(self, batch, logs={}):
-        if self._batch % self._each_epoch == 0:
+            data = json.dump(self._metrics, f)
             
-            file_name = self._config.output_folder+self._serial+'_weights%08d.h5' % self._batch
+        if self._epoch % self._each_epoch == 0:
+            
+            file_name = self._config.output_folder+self._serial+'_weights%08d.h5' % self._epoch
             print 'Salving weights at',file_name
             self._model.save_weights(file_name)
-        self._batch += 1
+        self._epoch += 1
